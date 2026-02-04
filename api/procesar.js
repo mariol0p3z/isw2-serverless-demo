ffd07ffbcb4245e376210387ffce57d5741e8791
Mario Lopez <158306854+mariol0p3z@users.noreply.github.com>
2026-02-03 20:26:01 -0600

    Actualizar procesar para manejo de error y timestamp

--- api/procesar.js
+++ api/procesar.js
@@ -1,7 +1,16 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
+  const timestamp = new Date().toISOString()
 
+  if(nombre == "error"){
+    return res.status(500).json({
+      error: "Nombre no puede ser error",
+      timestamp
+    })
+  }
+  
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+    timestamp
   });
 }
