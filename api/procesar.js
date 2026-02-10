7c84e0fb98b71ca438d38c90421bd3d0e093144a
Mario Lopez <158306854+mariol0p3z@users.noreply.github.com>
2026-02-09 19:57:34 -0600

    Validar error para nombre

--- api/procesar.js
+++ api/procesar.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
+  if(nombre === "error"){
+    return res.status(400).json({
+      error: "Nombre invalido"
+    })
+  }
   res.status(200).json({
     resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
     longitud: nombre.length
