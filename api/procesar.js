f1c71e0b47482c6ba4131988e7bfba67241feaae
Mario Lopez <158306854+mariol0p3z@users.noreply.github.com>
2026-02-09 18:18:58 -0600

    Remove error handling for 'error' name

--- api/procesar.js
+++ api/procesar.js
@@ -1,16 +1,7 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
-  const timestamp = new Date().toISOString()
 
-  if(nombre == "error"){
-    return res.status(500).json({
-      error: "Nombre no puede ser error",
-      timestamp
-    })
-  }
-  
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
-    timestamp
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
   });
 }
